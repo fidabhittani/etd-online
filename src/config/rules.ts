10387f5c2d95e8ce494422882eafcb7487833aa2
Irfan <muhammad.irfanali@gmail.com>
2019-11-03 19:37:16 +0500

    Initial values for test

--- src/config/rules.ts
+++ src/config/rules.ts
@@ -157,25 +157,25 @@ export const transferFormValidationRules = {
   representativeFName: []
 };
 
-const activateInitValues = true;
+const activateInitValues = false;
 
 export const initialValues = {
-  ownerType: activateInitValues ? "" : null,
-  ntn: activateInitValues ? "" : null,
-  cnic: activateInitValues ? "" : null,
-  passport: activateInitValues ? "" : null,
-  fatherHusbandName: activateInitValues ? "" : null,
-  contactNumber: activateInitValues ? "" : null,
-  otherContactNumber: activateInitValues ? "" : null,
-  presentAddress: activateInitValues ? "" : null,
-  presentAddressCity: activateInitValues ? "" : null,
-  presentAddressDistrict: activateInitValues ? "" : null,
-  permanentAddress: activateInitValues ? "" : null,
-  permanentAddressCity: activateInitValues ? "" : null,
-  permanentAddressDistrict: activateInitValues ? "" : null,
-  taxpayerType: activateInitValues ? "" : null,
-  representativeCnic: activateInitValues ? "" : null,
-  representativeMobile: activateInitValues ? "" : null,
-  representativeName: activateInitValues ? "" : null,
-  representativeFName: activateInitValues ? "" : null
+  ownerType: activateInitValues ? "INDIVIDUAL" : null,
+  ntn: activateInitValues ? "7677332" : null,
+  cnic: activateInitValues ? "1120188007492" : null,
+  passport: activateInitValues ? "DS-20192" : null,
+  fatherHusbandName: activateInitValues ? "ABDUL HAMID" : null,
+  contactNumber: activateInitValues ? "123123" : null,
+  otherContactNumber: activateInitValues ? "123123" : null,
+  presentAddress: activateInitValues ? "G13, Islamabad" : null,
+  presentAddressCity: activateInitValues ? "ISLAMABAD" : null,
+  presentAddressDistrict: activateInitValues ? "ISLAMABAD" : null,
+  permanentAddress: activateInitValues ? "G13, Islamabad" : null,
+  permanentAddressCity: activateInitValues ? "ISLAMABAD" : null,
+  permanentAddressDistrict: activateInitValues ? "ISLAMABAD" : null,
+  taxpayerType: activateInitValues ? "FILER" : null,
+  representativeCnic: activateInitValues ? "1234567890987" : null,
+  representativeMobile: activateInitValues ? "3210323" : null,
+  representativeName: activateInitValues ? "Ali" : null,
+  representativeFName: activateInitValues ? "Khan" : null
 };
